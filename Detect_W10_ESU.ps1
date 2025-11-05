490118e5ff9a4e9d2ad35851c4d4388482798ea1
Gene Moody <148409903+GeneMoody-Action1@users.noreply.github.com>
2025-11-05 15:16:44 -0600

    Update Detect_W10_ESU.ps1

--- Detect_W10_ESU.ps1
+++ Detect_W10_ESU.ps1
@@ -17,32 +17,105 @@
 # EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE
 # LIMITATIONS AND EXCLUSIONS MAY NOT APPLY TO YOU.
 
-$key = Get-CimInstance -ClassName SoftwareLicensingProduct |
-       Where-Object { $_.Description -match "ESU" } |
-       Select-Object Description, LicenseStatus, PartialProductKey
+function ConvertTo-DateTime {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Value,
+        [Parameter(Mandatory)]
+        [string]$Format,
+        [Parameter(Mandatory)]
+        [ValidateSet("Local", "UTC")]
+        [string]$TimeZone
+    )
+    try {
+        $current_culture = Get-Culture -ErrorAction Stop
+        $current_culture_date_time = [datetime]::Parse($Value, $current_culture)
+        if ($TimeZone -ceq 'UTC') {
+            $current_culture_date_time = $current_culture_date_time.ToUniversalTime()
+        }
+        if ($current_culture.LCID -ne 1033) {
+            $en_us_culture = [System.Globalization.CultureInfo]::CreateSpecificCulture('en-US')
+            $formatted_date_time = $current_culture_date_time.ToString($Format, $en_us_culture)
+        }
+        else {
+            $formatted_date_time = $current_culture_date_time.ToString($Format)
+        }
 
-$comp = Get-ComputerInfo
-
- if($null -ne $key) {
-     New-Object -Type PSCustomObject -Property $([ordered]@{
-            "Name" = $comp.WindowsProductName
-            "Version" = $comp.OsVersion
-            "Release" = $comp.OsDisplayVersion
-            "Edition" = $comp.WindowsEditionId
-            "Description" = ($Key.Description)
-            "LicenseStatus" = ($Key.LicenseStatus)
-            "PartialProductKey" = ($Key.PartialProductKey)
-            A1_Key              = "$($env:COMPUTERNAME)"
-        })
-    }else{
-     New-Object -Type PSCustomObject -Property $([ordered]@{
-            "Name" = $comp.WindowsProductName
-            "Version" = $comp.OsVersion
-            "Release" = $comp.OsDisplayVersion
-            "Edition" = $comp.WindowsEditionId
-            "Description" = ("NO ESU Applied")
-            "LicenseStatus" = ("N/A")
-            "PartialProductKey" = ("N/A")
-            A1_Key              = "$($env:COMPUTERNAME)"
-        })
+        return $formatted_date_time
     }
+    catch {}
+}
+
+function Get-ESUEligibilityDescription {
+    param (
+        [Parameter(Mandatory = $true)]
+        [int]$ESUEligibility
+    )
+
+    $ESUEligibilityDescription = "Unknown"
+
+    switch ($ESUEligibility) {
+        0 { $ESUEligibilityDescription = "Unknown. Feature is not enabled." }
+        1 { $ESUEligibilityDescription = "Ineligible" }
+        2 { $ESUEligibilityDescription = "Eligible" }
+        3 { $ESUEligibilityDescription = "Device Enrolled" }
+        5 { $ESUEligibilityDescription = "MSA Enrolled" }
+        8 { $ESUEligibilityDescription = "Login with Primary Account to Enroll" }
+        default { $ESUEligibilityDescription = "Unrecognized value: $ESUEligibility" }
+    }
+
+    return $ESUEligibilityDescription
+}
+
+function Get-ESUEligibilityResultDescription {
+    param (
+        [Parameter(Mandatory = $true)]
+        [int]$ESUEligibilityResult
+    )
+
+    $ESUEligibilityResultDescription = "Unknown"
+
+    switch ($ESUEligibilityResult) {
+        1 { $ESUEligibilityResultDescription = "Success" }
+        3 { $ESUEligibilityResultDescription = "Non-Consumer Edition" }
+        4 { $ESUEligibilityResultDescription = "Commercial Device" }
+        5 { $ESUEligibilityResultDescription = "Non-Admin Account" }
+        6 { $ESUEligibilityResultDescription = "Child Account" }
+        7 { $ESUEligibilityResultDescription = "User Region is Embargoed" }
+        8 { $ESUEligibilityResultDescription = "Azure Device" }
+       11 { $ESUEligibilityResultDescription = "Unknown. Feature is not enabled." }
+        default { $ESUEligibilityResultDescription = "Unrecognized value: $ESUEligibility" }
+    }
+
+    return $ESUEligibilityResultDescription
+}
+
+Start-Process -FilePath "$env:SystemRoot\System32\ClipESUConsumer.exe" -ArgumentList "-evaluateEligibility" -Wait -NoNewWindow
+
+$result = New-Object System.Collections.ArrayList;
+$currentOutput = "" | Select-Object "Endpoint Name", "ESUEligibility", "ESUEligibilityResult", "Timestamp", A1_Key;
+$currentOutput."Endpoint Name" = $env:COMPUTERNAME;
+
+
+$regPath = "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows\ConsumerESU"
+$values = Get-ItemProperty -Path $regPath -Name ESUEligibility, ESUEligibilityResult -ErrorAction SilentlyContinue
+
+if ($values) {
+
+	$currentOutput."ESUEligibility" = Get-ESUEligibilityDescription -ESUEligibility $values.ESUEligibility
+	$currentOutput."ESUEligibilityResult" = Get-ESUEligibilityResultDescription -ESUEligibilityResult $values.ESUEligibilityResult
+		
+} else {
+    $currentOutput."ESUEligibilityResult" = $currentOutput."ESUEligibility" = "The ConsumerESU registry path or values were not found.";
+}
+
+$date_time_now = [datetime]::Now.ToLocalTime()
+$execution_date_time_format = 'MMM dd, yyyy hh:mm:ss tt zzz'
+$currentOutput."Timestamp" = ConvertTo-DateTime -Value $date_time_now.ToString() -Format $execution_date_time_format -TimeZone Local
+
+
+$currentOutput.A1_Key = $env:COMPUTERNAME;
+
+$result.Add($currentOutput) | Out-Null;
+
+$result;
